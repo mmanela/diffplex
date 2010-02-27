1ff3644c27b96de6983d46bea568dcb5a7f5474a
Matthew Manela <mmanela@gmail.com>
2010-02-27 14:09:43 -0800

    Added documentaion on the DiffResult and SideBySideDiffModels

--- DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
+++ DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
@@ -1,5 +1,8 @@
 ﻿namespace DiffPlex.DiffBuilder.Model
 {
+    /// <summary>
+    /// A model which represents differences between to texts to be shown side by side
+    /// </summary>
     public class SideBySideDiffModel
     {
         public DiffPaneModel OldText { get; private set; }
--- DiffPlex/Model/DiffResult.cs
+++ DiffPlex/Model/DiffResult.cs
@@ -2,12 +2,25 @@
 
 namespace DiffPlex.Model
 {
+    /// <summary>
+    /// The result of diffing two peices of text
+    /// </summary>
     public class DiffResult
     {
+        /// <summary>
+        /// The chunked peices of the old text
+        /// </summary>
         public string[] PiecesOld { get; private set; }
 
+        /// <summary>
+        /// The chunked peices of the new text
+        /// </summary>
         public string[] PiecesNew { get; private set; }
 
+
+        /// <summary>
+        /// A collection of DiffBlocks which details deletions and insertions
+        /// </summary>
         public IList<DiffBlock> DiffBlocks { get; private set; }
 
         public DiffResult(string[] peicesOld, string[] piecesNew, IList<DiffBlock> blocks)

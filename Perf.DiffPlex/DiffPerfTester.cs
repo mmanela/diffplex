0069e444c35cf964d3d6fad51481433d236c8f1b
mmanela <none@none>
2010-04-19 08:41:41 -0700

    Added performance testing project

---
+++ Perf.DiffPlex/DiffPerfTester.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DiffPlex;
+
+namespace Perf.DiffPlex
+{
+    class DiffPerfTester
+    {
+        private readonly IDiffer differ;
+        const int MaxLineLength = 150;
+        const double DifferenceAmount = 0.2;
+        const int MaxLines = 8000;
+
+        public DiffPerfTester()
+        {
+            differ = new Differ();
+        }
+
+        public void Run()
+        {
+            var oldLines = GenerateLines(MaxLines);
+            var newLines = MakeDifferent(oldLines, DifferenceAmount);
+            var oldText = Implode(oldLines, Environment.NewLine);
+            var newText = Implode(newLines, Environment.NewLine);
+            new PerfTester().Run(() => differ.CreateLineDiffs(oldText, newText, false));
+        }
+
+
+        private string Implode<T>(IEnumerable<T> enumerable, string delim)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
+            if (delim == null) throw new ArgumentNullException("delim");
+
+            bool loopedAtLeaseOnce = false;
+            StringBuilder result = new StringBuilder();
+            foreach (var item in enumerable)
+            {
+                loopedAtLeaseOnce = true;
+                result.Append(item + delim);
+            }
+
+            if (loopedAtLeaseOnce)
+                return result.Remove(result.Length - delim.Length, delim.Length).ToString();
+            else
+                return String.Empty;
+        }
+
+        private IList<string> MakeDifferent(IList<string> lines, double differenceAmount)
+        {
+            var random = new Random();
+            var newLines = new List<string>();
+            foreach (var i in Enumerable.Range(0, lines.Count))
+            {
+                if(random.NextDouble() <= differenceAmount)
+                {
+                    // Either delete line or add different one
+                    if(random.Next(2) % 2 == 1)
+                    {
+                        newLines.Add(RandomString(MaxLineLength));
+                    }
+
+                }
+                else
+                {
+                    newLines.Add(lines[i]);
+                }
+            }
+
+            return newLines;
+
+        }
+
+        private IList<string> GenerateLines(int lines)
+        {
+            return Enumerable.Range(0, lines).Select(i => RandomString(MaxLineLength)).ToList();
+        }
+
+        private string RandomString(int maxLength)
+        {
+            var builder = new StringBuilder();
+            var random = new Random();
+            char ch;
+            foreach (var i in Enumerable.Range(0, random.Next(0, maxLength)))
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            if (random.Next(2) % 2 == 0)
+                return builder.ToString().ToLower();
+
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ Perf.DiffPlex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Perf.DiffPlex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"DiffPlex Perf Tester");
+
+            new DiffPerfTester().Run();
+            Console.WriteLine();
+        }
+    }
+}

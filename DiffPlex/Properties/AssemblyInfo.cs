cb95b0789262594b036cca11e7efa54807c698e2
Andrew Arnott <andarno@microsoft.com>
2014-09-13 21:44:27 -0700

    Converts the DiffPlex project to a PCL.
    This targets .NET 4.0 as it did originally, and it targets SL5, so that the SL5 project is no longer required.
    It also targets Windows 8.0, Windows Phone 8.0 and Windows Phone Appx 8.1.
    
    So in short, this greatly expands the allowed users of this library and with fewer projects required.
    It also works around the NuProj build issue since referencing the SL5 project is no longer required.

--- DiffPlex/Properties/AssemblyInfo.cs
+++ DiffPlex/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3641ba1e-29aa-45ab-b7be-75e42ad5d127")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version

9b8dec24bfaebd5a7b4e0adca56ae46b7df74013
Matthew Manela <mmanela@gmail.com>
2016-11-23 08:28:47 -0800

    remove browserlink which led to stack overflow

--- WebDiffer/Default.aspx.cs
+++ WebDiffer/Default.aspx.cs
@@ -8,7 +8,6 @@ namespace DiffPlex.Web
     {
         public void Page_Load(object sender, System.EventArgs e)
         {
-            HttpContext.Current.RewritePath(Request.ApplicationPath, false);
             IHttpHandler httpHandler = new MvcHttpHandler();
             httpHandler.ProcessRequest(HttpContext.Current);
         }

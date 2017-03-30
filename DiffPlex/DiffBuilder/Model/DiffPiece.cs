ff6a0d325de86183f102bfb10f6e203381e6d87c
Drew Noakes <git@drewnoakes.com>
2017-03-30 21:56:10 +0100

    Move assignment to initialiser

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -16,14 +16,13 @@ namespace DiffPlex.DiffBuilder.Model
         public ChangeType Type { get; set; }
         public int? Position { get; set; }
         public string Text { get; set; }
-        public List<DiffPiece> SubPieces { get; set; }
+        public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
 
         public DiffPiece(string text, ChangeType type, int? position = null)
         {
             Text = text;
             Position = position;
             Type = type;
-            SubPieces = new List<DiffPiece>();
         }
 
         public DiffPiece()

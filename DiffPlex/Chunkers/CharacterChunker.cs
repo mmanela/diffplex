3eacd05dad6d00c065b005c0ab0ac7a6eed93e32
Cezary Piątek <cezarypiatek@live.com>
2019-11-23 16:10:12 +0100

    #47: Refactor - introduce IChunker interface

---
+++ DiffPlex/Chunkers/CharacterChunker.cs
@@ -0,0 +1,12 @@
+﻿namespace DiffPlex.Chunkers
+{
+    public class CharacterChunker:IChunker
+    {
+        public string[] Chunk(string text)
+        {
+            var s = new string[text.Length];
+            for (int i = 0; i < text.Length; i++) s[i] = text[i].ToString();
+            return s;
+        }
+    }
+}

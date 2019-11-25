88cd1594d2dbeeeaf7a2dadb1bccc52a9f02357a
Cezary Piątek <cezarypiatek@live.com>
2019-11-25 22:17:13 +0100

    #47: Mark redundant methods in IDiffer as obsolete

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -8,13 +8,28 @@ namespace DiffPlex
     /// </summary>
     public interface IDiffer
     {
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
+        
+        [Obsolete("Use CreateDiffs method instead", false)]
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
+        [Obsolete("Use CreateDiffs method instead", false)] 
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, bool ignoreCase, Func<string, string[]> chunker);
 
         /// <summary>

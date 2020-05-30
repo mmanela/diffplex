7a01d9eaff612a340a5fdb7c7ce6a7914deddec4
Matthew Manela <mmanela@gmail.com>
2020-05-30 10:56:37 -0700

    Rev version

--- package.ps1
+++ package.ps1
@@ -2,13 +2,13 @@ dotnet build -c Release
 
 push-location DiffPlex
 
-dotnet pack -c Release
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 pop-location
 
 
 push-location DiffPlex.Wpf
 
-dotnet pack -c Release
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 pop-location

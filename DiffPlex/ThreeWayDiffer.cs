4e04034c5ee4fcb6e6e6c14caf25c4da39da8f79
Matthew Manela <mmanela@users.noreply.github.com>
2025-07-18 08:39:10 -0700

    format

--- DiffPlex/ThreeWayDiffer.cs
+++ DiffPlex/ThreeWayDiffer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using DiffPlex.Chunkers;
 using DiffPlex.Model;
 
 namespace DiffPlex
@@ -15,33 +14,33 @@ namespace DiffPlex
 
         private readonly IDiffer _differ = Differ.Instance;
 
-        public ThreeWayDiffResult CreateDiffs(string baseText, string oldText, string newText, 
+        public ThreeWayDiffResult CreateDiffs(string baseText, string oldText, string newText,
         bool ignoreWhiteSpace, bool ignoreCase, IChunker chunker)
         {
-        if (baseText == null) throw new ArgumentNullException(nameof(baseText));
-        if (oldText == null) throw new ArgumentNullException(nameof(oldText));
-        if (newText == null) throw new ArgumentNullException(nameof(newText));
-        if (chunker == null) throw new ArgumentNullException(nameof(chunker));
+            if (baseText == null) throw new ArgumentNullException(nameof(baseText));
+            if (oldText == null) throw new ArgumentNullException(nameof(oldText));
+            if (newText == null) throw new ArgumentNullException(nameof(newText));
+            if (chunker == null) throw new ArgumentNullException(nameof(chunker));
 
-        var basePieces = chunker.Chunk(baseText);
-        var oldPieces = chunker.Chunk(oldText);
-        var newPieces = chunker.Chunk(newText);
+            var basePieces = chunker.Chunk(baseText);
+            var oldPieces = chunker.Chunk(oldText);
+            var newPieces = chunker.Chunk(newText);
 
-        // Create two-way diffs: base->old and base->new
-        var baseToOld = _differ.CreateDiffs(baseText, oldText, ignoreWhiteSpace, ignoreCase, chunker);
-        var baseToNew = _differ.CreateDiffs(baseText, newText, ignoreWhiteSpace, ignoreCase, chunker);
+            // Create two-way diffs: base->old and base->new
+            var baseToOld = _differ.CreateDiffs(baseText, oldText, ignoreWhiteSpace, ignoreCase, chunker);
+            var baseToNew = _differ.CreateDiffs(baseText, newText, ignoreWhiteSpace, ignoreCase, chunker);
 
-        var threeWayBlocks = CreateThreeWayDiffBlocks(basePieces, oldPieces, newPieces, 
-        baseToOld, baseToNew, ignoreWhiteSpace, ignoreCase);
+            var threeWayBlocks = CreateThreeWayDiffBlocks(basePieces, oldPieces, newPieces,
+            baseToOld, baseToNew, ignoreWhiteSpace, ignoreCase);
 
-        return new ThreeWayDiffResult(basePieces, oldPieces, newPieces, threeWayBlocks);
+            return new ThreeWayDiffResult(basePieces, oldPieces, newPieces, threeWayBlocks);
         }
 
-        public ThreeWayMergeResult CreateMerge(string baseText, string oldText, string newText, 
+        public ThreeWayMergeResult CreateMerge(string baseText, string oldText, string newText,
         bool ignoreWhiteSpace, bool ignoreCase, IChunker chunker)
         {
-        var diffResult = CreateDiffs(baseText, oldText, newText, ignoreWhiteSpace, ignoreCase, chunker);
-            
+            var diffResult = CreateDiffs(baseText, oldText, newText, ignoreWhiteSpace, ignoreCase, chunker);
+
             var mergedPieces = new List<string>();
             var conflictBlocks = new List<ThreeWayConflictBlock>();
             var isSuccessful = true;
@@ -52,75 +51,75 @@ namespace DiffPlex
 
             foreach (var block in diffResult.DiffBlocks)
             {
-            // Add unchanged content before this block
-            while (baseIndex < block.BaseStart)
-            {
-            mergedPieces.Add(diffResult.PiecesBase[baseIndex]);
-            baseIndex++;
-            oldIndex++;
-            newIndex++;
-            }
+                // Add unchanged content before this block
+                while (baseIndex < block.BaseStart)
+                {
+                    mergedPieces.Add(diffResult.PiecesBase[baseIndex]);
+                    baseIndex++;
+                    oldIndex++;
+                    newIndex++;
+                }
 
-            switch (block.ChangeType)
-            {
-            case ThreeWayChangeType.Unchanged:
-            // Add base content (all are the same)
-            for (int i = 0; i < block.BaseCount; i++)
-            {
-            mergedPieces.Add(diffResult.PiecesBase[baseIndex + i]);
-            }
-            break;
+                switch (block.ChangeType)
+                {
+                    case ThreeWayChangeType.Unchanged:
+                        // Add base content (all are the same)
+                        for (int i = 0; i < block.BaseCount; i++)
+                        {
+                            mergedPieces.Add(diffResult.PiecesBase[baseIndex + i]);
+                        }
+                        break;
 
-            case ThreeWayChangeType.OldOnly:
-            // Take old version
-            for (int i = 0; i < block.OldCount; i++)
-            {
-            mergedPieces.Add(diffResult.PiecesOld[oldIndex + i]);
-            }
-            break;
+                    case ThreeWayChangeType.OldOnly:
+                        // Take old version
+                        for (int i = 0; i < block.OldCount; i++)
+                        {
+                            mergedPieces.Add(diffResult.PiecesOld[oldIndex + i]);
+                        }
+                        break;
 
-            case ThreeWayChangeType.NewOnly:
-            // Take new version
-            for (int i = 0; i < block.NewCount; i++)
-            {
-            mergedPieces.Add(diffResult.PiecesNew[newIndex + i]);
-            }
-            break;
+                    case ThreeWayChangeType.NewOnly:
+                        // Take new version
+                        for (int i = 0; i < block.NewCount; i++)
+                        {
+                            mergedPieces.Add(diffResult.PiecesNew[newIndex + i]);
+                        }
+                        break;
 
-            case ThreeWayChangeType.BothSame:
-            // Both made the same change, take either (we'll take old)
-            for (int i = 0; i < block.OldCount; i++)
-            {
-            mergedPieces.Add(diffResult.PiecesOld[oldIndex + i]);
-            }
-            break;
+                    case ThreeWayChangeType.BothSame:
+                        // Both made the same change, take either (we'll take old)
+                        for (int i = 0; i < block.OldCount; i++)
+                        {
+                            mergedPieces.Add(diffResult.PiecesOld[oldIndex + i]);
+                        }
+                        break;
 
-            case ThreeWayChangeType.Conflict:
-            // Create conflict block
-            var basePieces = diffResult.PiecesBase.Skip(baseIndex).Take(block.BaseCount).ToList();
-            var oldPieces = diffResult.PiecesOld.Skip(oldIndex).Take(block.OldCount).ToList();
-            var newPieces = diffResult.PiecesNew.Skip(newIndex).Take(block.NewCount).ToList();
-            
-            var conflictBlock = new ThreeWayConflictBlock(mergedPieces.Count, basePieces, 
-            oldPieces, newPieces, block);
-            conflictBlocks.Add(conflictBlock);
+                    case ThreeWayChangeType.Conflict:
+                        // Create conflict block
+                        var basePieces = diffResult.PiecesBase.Skip(baseIndex).Take(block.BaseCount).ToList();
+                        var oldPieces = diffResult.PiecesOld.Skip(oldIndex).Take(block.OldCount).ToList();
+                        var newPieces = diffResult.PiecesNew.Skip(newIndex).Take(block.NewCount).ToList();
 
-            // Add conflict markers
-            mergedPieces.Add("<<<<<<< old");
-            mergedPieces.AddRange(oldPieces);
-            mergedPieces.Add("||||||| base");
-            mergedPieces.AddRange(basePieces);
-            mergedPieces.Add("=======");
-            mergedPieces.AddRange(newPieces);
-            mergedPieces.Add(">>>>>>> new");
+                        var conflictBlock = new ThreeWayConflictBlock(mergedPieces.Count, basePieces,
+                        oldPieces, newPieces, block);
+                        conflictBlocks.Add(conflictBlock);
 
-            isSuccessful = false;
-            break;
-            }
+                        // Add conflict markers
+                        mergedPieces.Add("<<<<<<< old");
+                        mergedPieces.AddRange(oldPieces);
+                        mergedPieces.Add("||||||| base");
+                        mergedPieces.AddRange(basePieces);
+                        mergedPieces.Add("=======");
+                        mergedPieces.AddRange(newPieces);
+                        mergedPieces.Add(">>>>>>> new");
 
-            baseIndex += block.BaseCount;
-            oldIndex += block.OldCount;
-            newIndex += block.NewCount;
+                        isSuccessful = false;
+                        break;
+                }
+
+                baseIndex += block.BaseCount;
+                oldIndex += block.OldCount;
+                newIndex += block.NewCount;
             }
 
             // Add remaining unchanged content
@@ -133,18 +132,18 @@ namespace DiffPlex
             return new ThreeWayMergeResult(mergedPieces, isSuccessful, conflictBlocks, diffResult);
         }
 
-        private List<ThreeWayDiffBlock> CreateThreeWayDiffBlocks(IReadOnlyList<string> basePieces, 
+        private List<ThreeWayDiffBlock> CreateThreeWayDiffBlocks(IReadOnlyList<string> basePieces,
             IReadOnlyList<string> oldPieces, IReadOnlyList<string> newPieces,
             DiffResult baseToOld, DiffResult baseToNew, bool ignoreWhiteSpace, bool ignoreCase)
         {
             var blocks = new List<ThreeWayDiffBlock>();
-            
+
             // If no changes, return single unchanged block
             if (baseToOld.DiffBlocks.Count == 0 && baseToNew.DiffBlocks.Count == 0)
             {
                 if (basePieces.Count > 0)
                 {
-                    blocks.Add(new ThreeWayDiffBlock(0, basePieces.Count, 0, basePieces.Count, 
+                    blocks.Add(new ThreeWayDiffBlock(0, basePieces.Count, 0, basePieces.Count,
                         0, basePieces.Count, ThreeWayChangeType.Unchanged));
                 }
                 return blocks;
@@ -159,9 +158,9 @@ namespace DiffPlex
 
             while (baseIndex < basePieces.Count)
             {
-                var nextOldChange = oldBlockIndex < baseToOld.DiffBlocks.Count 
+                var nextOldChange = oldBlockIndex < baseToOld.DiffBlocks.Count
                     ? baseToOld.DiffBlocks[oldBlockIndex].DeleteStartA : int.MaxValue;
-                var nextNewChange = newBlockIndex < baseToNew.DiffBlocks.Count 
+                var nextNewChange = newBlockIndex < baseToNew.DiffBlocks.Count
                     ? baseToNew.DiffBlocks[newBlockIndex].DeleteStartA : int.MaxValue;
                 var nextChange = Math.Min(nextOldChange, nextNewChange);
 
@@ -169,9 +168,9 @@ namespace DiffPlex
                 if (baseIndex < nextChange && nextChange != int.MaxValue)
                 {
                     var unchangedCount = nextChange - baseIndex;
-                    blocks.Add(new ThreeWayDiffBlock(baseIndex, unchangedCount, oldIndex, unchangedCount, 
+                    blocks.Add(new ThreeWayDiffBlock(baseIndex, unchangedCount, oldIndex, unchangedCount,
                         newIndex, unchangedCount, ThreeWayChangeType.Unchanged));
-                    
+
                     baseIndex += unchangedCount;
                     oldIndex += unchangedCount;
                     newIndex += unchangedCount;
@@ -187,7 +186,7 @@ namespace DiffPlex
                     var changeType = DetermineChangeType(basePieces, oldPieces, newPieces,
                         oldBlock, newBlock, ignoreWhiteSpace, ignoreCase);
 
-                    blocks.Add(new ThreeWayDiffBlock(baseIndex, oldBlock.DeleteCountA, 
+                    blocks.Add(new ThreeWayDiffBlock(baseIndex, oldBlock.DeleteCountA,
                         oldIndex, oldBlock.InsertCountB,
                         newIndex, newBlock.InsertCountB, changeType));
 
@@ -202,7 +201,7 @@ namespace DiffPlex
                 {
                     // Only old has change
                     var oldBlock = baseToOld.DiffBlocks[oldBlockIndex];
-                    blocks.Add(new ThreeWayDiffBlock(baseIndex, oldBlock.DeleteCountA, 
+                    blocks.Add(new ThreeWayDiffBlock(baseIndex, oldBlock.DeleteCountA,
                         oldIndex, oldBlock.InsertCountB,
                         newIndex, oldBlock.DeleteCountA, ThreeWayChangeType.OldOnly));
 
@@ -216,7 +215,7 @@ namespace DiffPlex
                 {
                     // Only new has change
                     var newBlock = baseToNew.DiffBlocks[newBlockIndex];
-                    blocks.Add(new ThreeWayDiffBlock(baseIndex, newBlock.DeleteCountA, 
+                    blocks.Add(new ThreeWayDiffBlock(baseIndex, newBlock.DeleteCountA,
                         oldIndex, newBlock.DeleteCountA,
                         newIndex, newBlock.InsertCountB, ThreeWayChangeType.NewOnly));
 
@@ -232,7 +231,7 @@ namespace DiffPlex
                     var remainingCount = basePieces.Count - baseIndex;
                     if (remainingCount > 0)
                     {
-                        blocks.Add(new ThreeWayDiffBlock(baseIndex, remainingCount, oldIndex, remainingCount, 
+                        blocks.Add(new ThreeWayDiffBlock(baseIndex, remainingCount, oldIndex, remainingCount,
                             newIndex, remainingCount, ThreeWayChangeType.Unchanged));
                     }
                     break;
@@ -242,7 +241,7 @@ namespace DiffPlex
             return blocks;
         }
 
-        private ThreeWayChangeType DetermineChangeType(IReadOnlyList<string> basePieces, 
+        private ThreeWayChangeType DetermineChangeType(IReadOnlyList<string> basePieces,
             IReadOnlyList<string> oldPieces, IReadOnlyList<string> newPieces,
             DiffBlock oldBlock, DiffBlock newBlock, bool ignoreWhiteSpace, bool ignoreCase)
         {
@@ -257,7 +256,7 @@ namespace DiffPlex
 
             // Compare the changes
             var comparer = CreateStringComparer(ignoreWhiteSpace, ignoreCase);
-            
+
             if (oldContent.SequenceEqual(newContent, comparer))
             {
                 return ThreeWayChangeType.BothSame;
@@ -292,7 +291,7 @@ namespace DiffPlex
                 var stringX = _ignoreWhiteSpace ? x.Trim() : x;
                 var stringY = _ignoreWhiteSpace ? y.Trim() : y;
 
-                return string.Equals(stringX, stringY, 
+                return string.Equals(stringX, stringY,
                     _ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
             }
 
